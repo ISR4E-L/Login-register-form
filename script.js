892cb123e806a0af88de5a4eb4c99c66f76a2d12
ISR4E-L <you@example.com>
2026-02-14 18:18:45 +0200

    Initial commit - login/register form with Israel styling

---
+++ script.js
@@ -0,0 +1,14 @@
+let loginGroup    = document.getElementById("easy-login");
+let registerGroup = document.getElementById("easy-register");
+let slider        = document.getElementById("easy-slider");
+function easyRegister() {
+    loginGroup.style.left    = "-450px";
+    registerGroup.style.left = "50px";
+    slider.style.left        = "110px";
+}
+function easyLogin() {
+    loginGroup.style.left    = "50px";
+    registerGroup.style.left = "450px";
+    slider.style.left        = "0px";
+}
+easyLogin();
